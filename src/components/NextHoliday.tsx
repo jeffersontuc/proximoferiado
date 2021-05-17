21044ee144ce474830a656baf8a24305219bd38b
Jefferson <jefiim.ad@gmail.com>
2021-05-17 00:50:08 -0300

    shows the next holiday

---
+++ src/components/NextHoliday.tsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react"
+
+const NextHoliday = () => {
+  return (
+    <Fragment>
+      <div className="holiday flex flex-column">
+        <div className="date">3 de Junho</div>
+        <div className="week-day">quinta feira</div>
+      </div>
+      <footer className="footer">Próximo feriado | Corpus Christi</footer>
+    </Fragment>
+  )
+}
+
+export default NextHoliday
